00161a2df0888926bf8fa091186e484d18999a8c
jompahl <44161518+jompahl@users.noreply.github.com>
2020-11-28 13:27:07 +0100

    Add files via upload

--- Marina.java
+++ Marina.java
@@ -2,64 +2,75 @@ import java.util.ArrayList;
 
 public class Marina {
 
-    private String adress;
-    private String name;
-    private ArrayList<Mooring> mooringList = new ArrayList<Mooring>();
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void addMooring(Mooring p) {
-        mooringList.add(p);
-    }
-
-    public Mooring findMooring(int nbr) {
-        for(Mooring tmpMooring : mooringList) {
-            if(nbr == tmpMooring.getNbr()) {
-                return tmpMooring;
-            }
-        }
-        return null;
-    }
-
-    public void addTenant(Tenant h, int mooringNbr) {
-        Mooring mooring = findMooring(mooringNbr);
-
-        if(mooring != null) {
-            mooring.addTenant(h);
-        }
-    }
-
-    public double getRentsForMarina() {
-        double totalRent = 0;
-
-        for (Mooring tmpMooring : mooringList) {
-            totalRent += tmpMooring.getRent();
-        }
-        return totalRent;
-    }
-
-    public ArrayList<Mooring> getAvailableMoorings() {
-        ArrayList<Mooring> availableMoorings = new ArrayList<>();
-
-        for(Mooring tmpMooring : mooringList) {
-            if(tmpMooring.getTenantList().isEmpty()) {
-                availableMoorings.add(tmpMooring);
-            }
-        }
-        return availableMoorings;
-    }
+	private String address;
+	private String name;
+	private ArrayList<Mooring> mooringList = new ArrayList<>();
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Mooring> getMooringList() {
+		return mooringList;
+	}
+	public void setMooringList(ArrayList<Mooring> mooringList) {
+		this.mooringList = mooringList;
+	}	
+	
+	public void addMooring(Mooring p) {
+		mooringList.add(p);
+	}
+	
+	public Mooring findMooring(int nbr) {
+		for(Mooring tmpMooring : mooringList) {
+			if(tmpMooring.getNbr() == nbr) {
+				return tmpMooring;
+			}
+		}
+		return null;
+	}
+	
+	public void addTenant(Tenant h, int mooringNbr) {
+		Mooring tmpMooring = this.findMooring(mooringNbr);
+		
+		if(tmpMooring != null) {
+			tmpMooring.addTenant(h);
+			h.setMooring(tmpMooring);
+		}
+	}
+	
+	public ArrayList<Mooring> getAvailableMoorings() {
+		ArrayList<Mooring> tmpList = new ArrayList<>();
+		
+		for(Mooring tmpMooring : mooringList) {
+			if(tmpMooring.getTenantList().isEmpty()) {
+				tmpList.add(tmpMooring);
+			}
+		}
+		return tmpList;
+	}
+	
+	public double getRentsForMarina() {
+		double totalRent = 0;
+		
+		for(Mooring tmpMooring : mooringList) {
+			if(!tmpMooring.getTenantList().isEmpty()) {
+				totalRent += tmpMooring.getRent();
+			}
+		}
+		return totalRent;
+	}
+	
 }
+
+
+
+
